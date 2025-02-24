127c141df0cdc9f00acbc851433ec1d7bc259fdb
rutvikshah <rutvikjshah@gmail.com>
2025-02-24 22:04:04 +0530

    Setup the model names

--- src/ai_agent.py
+++ src/ai_agent.py
@@ -6,3 +6,6 @@ load_dotenv()
 from langchain_groq import ChatGroq
 from langchain_openai import OpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
+
+openai_llm = OpenAI(model="gpt-4o-mini")
+groq_llm = ChatGroq(model="llama-3.3-70b-versatile")
