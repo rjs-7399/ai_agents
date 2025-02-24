25454032b19033f96ac7ca6147ea12905867a3e0
rutvikshah <rutvikjshah@gmail.com>
2025-02-24 22:57:41 +0530

    Added backend API code

---
+++ financial_analyst_ai_agent_with_fast_api/src/backend.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel
+from typing import List
+from fastapi import FastAPI
+from ai_agent import get_response_from_ai_agent
+import uvicorn
+
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool
+
+ALLOWED_MODEL_NAMES = [
+    "llama3-70b-8192",
+    "mixtral-8x7b-32768",
+    "llama-3.3-70b-versatile",
+    "gpt-4o-mini"
+]
+app = FastAPI(title="Financial AI Agent")
+
+@app.post("/chat")
+def chat_endpoint(request: RequestState):
+    """
+    API Endpoint to interact with the Chatbot using LangGraph and Search Tools.
+    It dynamically selects the model specified in the request.
+    """
+    if request.model_name not in ALLOWED_MODEL_NAMES:
+        return {
+            "error": "Invalid model name. Kindly select a valid AI model"
+        }
+
+    llm_id = request.model_name
+    query = request.messages
+    allow_search = request.allow_search
+    system_prompt = request.system_prompt
+    provider = request.model_provider
+
+    response = get_response_from_ai_agent(
+        llm_id, query, allow_search, system_prompt, provider
+    )
+    return response
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9999)
