04d78568ace04673eb377714a57e6a06fcb5f0dc
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:20:56 +0530

    Added tool to search the cab based on location, price, time

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -1,16 +1,16 @@
-import re, requests
+import re
 import numpy as np
 import openai
 from langchain_core.tools import tool
 from dotenv import load_dotenv
-import sqlite3
-from datetime import date, datetime
-from typing import Optional
 import os, shutil, sqlite3
 import pandas as pd
 import requests
 import pytz
 from langchain_core.runnables import RunnableConfig
+from datetime import date, datetime
+from typing import Optional, Union
+
 
 load_dotenv()
 
@@ -300,3 +300,46 @@ def cancel_ticket(ticket_no: str, *, config: RunnableConfig) -> str:
     cursor.close()
     conn.close()
     return "Ticket successfully cancelled."
+
+
+@tool
+def search_car_rentals(
+    location: Optional[str] = None,
+    name: Optional[str] = None,
+    price_tier: Optional[str] = None,
+    start_date: Optional[Union[datetime, date]] = None,
+    end_date: Optional[Union[datetime, date]] = None,
+) -> list[dict]:
+    """
+    Search for car rentals based on location, name, price tier, start date, and end date.
+
+    Args:
+        location (Optional[str]): The location of the car rental. Defaults to None.
+        name (Optional[str]): The name of the car rental company. Defaults to None.
+        price_tier (Optional[str]): The price tier of the car rental. Defaults to None.
+        start_date (Optional[Union[datetime, date]]): The start date of the car rental. Defaults to None.
+        end_date (Optional[Union[datetime, date]]): The end date of the car rental. Defaults to None.
+
+    Returns:
+        list[dict]: A list of car rental dictionaries matching the search criteria.
+    """
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM car_rentals WHERE 1=1"
+    params = []
+
+    if location:
+        query += " AND location LIKE ?"
+        params.append(f"%{location}%")
+    if name:
+        query += " AND name LIKE ?"
+        params.append(f"%{name}%")
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+
+    conn.close()
+
+    return [
+        dict(zip([column[0] for column in cursor.description], row)) for row in results
+    ]
