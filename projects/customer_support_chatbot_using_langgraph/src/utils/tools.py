d416b9750bd3a509936857264a872dec99b29c1c
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:41:27 +0530

    Added tool to cancel the excursion

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -370,7 +370,6 @@ def book_car_rental(rental_id: int) -> str:
         return f"No car rental found with ID {rental_id}."
 
 
-
 @tool
 def update_car_rental(
     rental_id: int,
@@ -666,6 +665,33 @@ def update_excursion(recommendation_id: int, details: str) -> str:
     if cursor.rowcount > 0:
         conn.close()
         return f"Trip recommendation {recommendation_id} successfully updated."
+    else:
+        conn.close()
+        return f"No trip recommendation found with ID {recommendation_id}."
+
+
+@tool
+def cancel_excursion(recommendation_id: int) -> str:
+    """
+    Cancel a trip recommendation by its ID.
+
+    Args:
+        recommendation_id (int): The ID of the trip recommendation to cancel.
+
+    Returns:
+        str: A message indicating whether the trip recommendation was successfully cancelled or not.
+    """
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "UPDATE trip_recommendations SET booked = 0 WHERE id = ?", (recommendation_id,)
+    )
+    conn.commit()
+
+    if cursor.rowcount > 0:
+        conn.close()
+        return f"Trip recommendation {recommendation_id} successfully cancelled."
     else:
         conn.close()
         return f"No trip recommendation found with ID {recommendation_id}."
