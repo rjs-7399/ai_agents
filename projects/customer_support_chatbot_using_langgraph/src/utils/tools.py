f4496d6158376087a99757ab84382a89e78e3300
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:23:12 +0530

    Added tool to update the cab by its ID with start and end date and time

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -368,3 +368,44 @@ def book_car_rental(rental_id: int) -> str:
     else:
         conn.close()
         return f"No car rental found with ID {rental_id}."
+
+
+
+@tool
+def update_car_rental(
+    rental_id: int,
+    start_date: Optional[Union[datetime, date]] = None,
+    end_date: Optional[Union[datetime, date]] = None,
+) -> str:
+    """
+    Update a car rental's start and end dates by its ID.
+
+    Args:
+        rental_id (int): The ID of the car rental to update.
+        start_date (Optional[Union[datetime, date]]): The new start date of the car rental. Defaults to None.
+        end_date (Optional[Union[datetime, date]]): The new end date of the car rental. Defaults to None.
+
+    Returns:
+        str: A message indicating whether the car rental was successfully updated or not.
+    """
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    if start_date:
+        cursor.execute(
+            "UPDATE car_rentals SET start_date = ? WHERE id = ?",
+            (start_date, rental_id),
+        )
+    if end_date:
+        cursor.execute(
+            "UPDATE car_rentals SET end_date = ? WHERE id = ?", (end_date, rental_id)
+        )
+
+    conn.commit()
+
+    if cursor.rowcount > 0:
+        conn.close()
+        return f"Car rental {rental_id} successfully updated."
+    else:
+        conn.close()
+        return f"No car rental found with ID {rental_id}."
