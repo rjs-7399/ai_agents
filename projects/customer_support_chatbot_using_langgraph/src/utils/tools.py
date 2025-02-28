60d809c0569c261e2c890c448f902aeea4c16cb6
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:26:40 +0530

    Added tool to search the hotel based on location, stay start date, stay end date

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -434,3 +434,47 @@ def cancel_car_rental(rental_id: int) -> str:
     else:
         conn.close()
         return f"No car rental found with ID {rental_id}."
+
+
+@tool
+def search_hotels(
+    location: Optional[str] = None,
+    name: Optional[str] = None,
+    price_tier: Optional[str] = None,
+    checkin_date: Optional[Union[datetime, date]] = None,
+    checkout_date: Optional[Union[datetime, date]] = None,
+) -> list[dict]:
+    """
+    Search for hotels based on location, name, price tier, check-in date, and check-out date.
+
+    Args:
+        location (Optional[str]): The location of the hotel. Defaults to None.
+        name (Optional[str]): The name of the hotel. Defaults to None.
+        price_tier (Optional[str]): The price tier of the hotel. Defaults to None. Examples: Midscale, Upper Midscale, Upscale, Luxury
+        checkin_date (Optional[Union[datetime, date]]): The check-in date of the hotel. Defaults to None.
+        checkout_date (Optional[Union[datetime, date]]): The check-out date of the hotel. Defaults to None.
+
+    Returns:
+        list[dict]: A list of hotel dictionaries matching the search criteria.
+    """
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM hotels WHERE 1=1"
+    params = []
+
+    if location:
+        query += " AND location LIKE ?"
+        params.append(f"%{location}%")
+    if name:
+        query += " AND name LIKE ?"
+        params.append(f"%{name}%")
+    # For the sake of this tutorial, we will let you match on any dates and price tier.
+    cursor.execute(query, params)
+    results = cursor.fetchall()
+
+    conn.close()
+
+    return [
+        dict(zip([column[0] for column in cursor.description], row)) for row in results
+    ]
