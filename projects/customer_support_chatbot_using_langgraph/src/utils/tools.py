f8eb3f1a1ba1a8a35ebf4f4a9062aa8ad7cfdcb0
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:18:41 +0530

    Added tool to cancel the user's ticket and remove it from DB

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -262,3 +262,41 @@ def update_ticket_to_new_flight(
     cursor.close()
     conn.close()
     return "Ticket successfully updated to new flight."
+
+
+@tool
+def cancel_ticket(ticket_no: str, *, config: RunnableConfig) -> str:
+    """Cancel the user's ticket and remove it from the database."""
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT flight_id FROM ticket_flights WHERE ticket_no = ?", (ticket_no,)
+    )
+    existing_ticket = cursor.fetchone()
+    if not existing_ticket:
+        cursor.close()
+        conn.close()
+        return "No existing ticket found for the given ticket number."
+
+    # Check the signed-in user actually has this ticket
+    cursor.execute(
+        "SELECT ticket_no FROM tickets WHERE ticket_no = ? AND passenger_id = ?",
+        (ticket_no, passenger_id),
+    )
+    current_ticket = cursor.fetchone()
+    if not current_ticket:
+        cursor.close()
+        conn.close()
+        return f"Current signed-in passenger with ID {passenger_id} not the owner of ticket {ticket_no}"
+
+    cursor.execute("DELETE FROM ticket_flights WHERE ticket_no = ?", (ticket_no,))
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return "Ticket successfully cancelled."
