d49b928677049434760363337f0cfad84c7372ec
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:15:32 +0530

    Added tool to search flights based on provided filters

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -155,3 +155,46 @@ def fetch_user_flight_information(config: RunnableConfig) -> list[dict]:
     conn.close()
 
     return results
+
+
+@tool
+def search_flights(
+    departure_airport: Optional[str] = None,
+    arrival_airport: Optional[str] = None,
+    start_time: Optional[date | datetime] = None,
+    end_time: Optional[date | datetime] = None,
+    limit: int = 20,
+) -> list[dict]:
+    """Search for flights based on departure airport, arrival airport, and departure time range."""
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM flights WHERE 1 = 1"
+    params = []
+
+    if departure_airport:
+        query += " AND departure_airport = ?"
+        params.append(departure_airport)
+
+    if arrival_airport:
+        query += " AND arrival_airport = ?"
+        params.append(arrival_airport)
+
+    if start_time:
+        query += " AND scheduled_departure >= ?"
+        params.append(start_time)
+
+    if end_time:
+        query += " AND scheduled_departure <= ?"
+        params.append(end_time)
+    query += " LIMIT ?"
+    params.append(limit)
+    cursor.execute(query, params)
+    rows = cursor.fetchall()
+    column_names = [column[0] for column in cursor.description]
+    results = [dict(zip(column_names, row)) for row in rows]
+
+    cursor.close()
+    conn.close()
+
+    return results
