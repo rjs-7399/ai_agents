2dff2bf29ab7dbc48a09569887e78fa7e201444e
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:58:46 +0530

    Added utility tool to print the events

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -724,3 +724,18 @@ def create_tool_node_with_fallback(tools: list) -> dict:
         [RunnableLambda(handle_tool_error)], exceptions_key="error"
     )
 
+
+def _print_event(event: dict, _printed: set, max_length=1500):
+    current_state = event.get("dialog_state")
+    if current_state:
+        print("Currently in:", current_state[-1])
+    message = event.get("messages")
+    if message:
+        if isinstance(message, list):
+            message = message[-1]
+        if message.id not in _printed:
+            msg_repr = message.pretty_repr(html=True)
+            if len(msg_repr) > max_length:
+                msg_repr = msg_repr[:max_length] + "... (truncated)"
+            print(msg_repr)
+            _printed.add(message.id)
