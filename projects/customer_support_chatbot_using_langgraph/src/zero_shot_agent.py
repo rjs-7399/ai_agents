92d191a60efa80dd08c03c10106b4a78de30cd87
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 12:48:24 +0530

    Added State in zero shot agent

---
+++ projects/customer_support_chatbot_using_langgraph/src/zero_shot_agent.py
@@ -0,0 +1,34 @@
+from langchain_core.runnables import Runnable, RunnableConfig
+from typing_extensions import TypedDict
+from typing import Annotated
+from datetime import date, datetime
+from langgraph.graph.message import AnyMessage, add_messages
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import END, StateGraph, START
+from langgraph.prebuilt import tools_condition
+from utils.tools import create_tool_node_with_fallback
+from utils.tools import (
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
