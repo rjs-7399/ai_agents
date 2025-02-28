91a8cc82d8fef942bdac3b8b725fe9878f8b202d
rutvikshah <rutvikjshah@gmail.com>
2025-02-28 11:17:25 +0530

    Added tool to update the ticket to new flight for user

--- projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
+++ projects/customer_support_chatbot_using_langgraph/src/utils/tools.py
@@ -198,3 +198,67 @@ def search_flights(
     conn.close()
 
     return results
+
+
+@tool
+def update_ticket_to_new_flight(
+    ticket_no: str, new_flight_id: int, *, config: RunnableConfig
+) -> str:
+    """Update the user's ticket to a new valid flight."""
+    configuration = config.get("configurable", {})
+    passenger_id = configuration.get("passenger_id", None)
+    if not passenger_id:
+        raise ValueError("No passenger ID configured.")
+
+    conn = sqlite3.connect(db)
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT departure_airport, arrival_airport, scheduled_departure FROM flights WHERE flight_id = ?",
+        (new_flight_id,),
+    )
+    new_flight = cursor.fetchone()
+    if not new_flight:
+        cursor.close()
+        conn.close()
+        return "Invalid new flight ID provided."
+    column_names = [column[0] for column in cursor.description]
+    new_flight_dict = dict(zip(column_names, new_flight))
+    timezone = pytz.timezone("Etc/GMT-3")
+    current_time = datetime.now(tz=timezone)
+    departure_time = datetime.strptime(
+        new_flight_dict["scheduled_departure"], "%Y-%m-%d %H:%M:%S.%f%z"
+    )
+    time_until = (departure_time - current_time).total_seconds()
+    if time_until < (3 * 3600):
+        return f"Not permitted to reschedule to a flight that is less than 3 hours from the current time. Selected flight is at {departure_time}."
+
+    cursor.execute(
+        "SELECT flight_id FROM ticket_flights WHERE ticket_no = ?", (ticket_no,)
+    )
+    current_flight = cursor.fetchone()
+    if not current_flight:
+        cursor.close()
+        conn.close()
+        return "No existing ticket found for the given ticket number."
+
+    # Check the signed-in user actually has this ticket
+    cursor.execute(
+        "SELECT * FROM tickets WHERE ticket_no = ? AND passenger_id = ?",
+        (ticket_no, passenger_id),
+    )
+    current_ticket = cursor.fetchone()
+    if not current_ticket:
+        cursor.close()
+        conn.close()
+        return f"Current signed-in passenger with ID {passenger_id} not the owner of ticket {ticket_no}"
+
+    cursor.execute(
+        "UPDATE ticket_flights SET flight_id = ? WHERE ticket_no = ?",
+        (new_flight_id, ticket_no),
+    )
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return "Ticket successfully updated to new flight."
