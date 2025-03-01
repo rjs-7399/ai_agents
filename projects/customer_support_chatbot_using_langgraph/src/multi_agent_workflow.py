717726489c5a34b35117132b280e3bea73071adc
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 19:20:57 +0530

    Added toool to mark the current task as complete or delegate the flow to main assistant

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -1,6 +1,7 @@
 from typing import Annotated, Literal, Optional
 from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
+from pydantic import BaseModel, Field
 
 
 def updated_dialog_stack(left: list[str], right: Optional[str]) -> list[str]:
@@ -26,3 +27,29 @@ class State(TypedDict):
         ],
         updated_dialog_stack,
     ]
+
+
+class CompleteOrEscalate(BaseModel):
+    """
+        A tool to mark the current task as completed and/or to escalate control of the dialog to the main assistant,
+        who can re-route the dialog based on the user's needs.
+    """
+    cancel: bool = True
+    reason: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "cancel": True,
+                "reason": "User changed their mind about the current Task",
+            },
+            "example 2": {
+                "cancel": True,
+                "reason": "I have fully complete the task.",
+            },
+            "example 3": {
+                "cancel": False,
+                "reason": "I need to seach the user's email or calendar for more information",
+            }
+        }
+
