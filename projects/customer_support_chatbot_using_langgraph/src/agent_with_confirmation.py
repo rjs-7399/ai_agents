b2c3d1a601a9ec30e732b3a673647373d101179a
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 14:50:51 +0530

    Defined Agent Assistant

--- projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
@@ -5,7 +5,48 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable, RunnableConfig
 from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
+from datetime import datetime
+from utils.tools import (
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
+
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
     user_info: str
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            result = self.runnable.invoke(state)
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
+
