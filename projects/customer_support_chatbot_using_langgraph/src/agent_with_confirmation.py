58ced7abaea63f97d9cd8fd9b73bab7e2031ad8e
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 15:13:29 +0530

    Added questions for agent with confirmation

--- projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Annotated
 from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
@@ -126,4 +127,23 @@ graph = builder.compile(
     checkpointer=memory,
     interrupt_before=["tools"],
 )
+
+from utils.tools import db, update_dates
+
+zero_shot_agent_questions = [
+    "Hi there, what time is my flight?",
+    "Am i allowed to update my flight to something sooner? I want to leave later today.",
+    "Update my flight to sometime next week then",
+    "The next available option is great",
+    "what about lodging and transportation?",
+    "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
+    "OK could you place a reservation for your recommended hotel? It sounds nice.",
+    "yes go ahead and book anything that's moderate expense and has availability.",
+    "Now for a car, what are my options?",
+    "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
+    "Cool so now what recommendations do you have on excursions?",
+    "Are they available while I'm there?",
+    "interesting - i like the museums, what options are there? ",
+    "OK great pick one and book it for my second day there.",
+]
 
