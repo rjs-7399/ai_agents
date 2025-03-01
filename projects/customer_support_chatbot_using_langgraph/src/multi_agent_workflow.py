ec045a1226883549423adeb399e4c6282cd0634a
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 20:37:36 +0530

    Added Primary Assistant Prompt

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -3,6 +3,7 @@ from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable, RunnableConfig
+from langchain_community.tools.tavily_search import TavilySearchResults
 from pydantic import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from datetime import datetime
@@ -50,12 +51,6 @@ class State(TypedDict):
         ],
         updated_dialog_stack,
     ]
-
-
-llm = ChatOpenAI(
-    temperature=0.0,
-    model="gpt-4o-mini"
-)
 
 
 class Assistant:
@@ -315,3 +310,24 @@ llm = ChatOpenAI(
     temperature=0.0,
     model="gpt-4o-mini"
 )
+
+
+primary_assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            "Your primary role is to search for flight information and company policies to answer customer queries. "
+            "If a customer requests to update or cancel a flight, book a car rental, book a hotel, or get trip recommendations, "
+            "delegate the task to the appropriate specialized assistant by invoking the corresponding tool. You are not able to make these types of changes yourself."
+            " Only the specialized assistants are given permission to do this for the user."
+            "The user is not aware of the different specialized assistants, so do not mention them; just quietly delegate through function calls. "
+            "Provide detailed information to the customer, and always double-check the database before concluding that information is unavailable. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user flight information:\n<Flights>\n{user_info}\n</Flights>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
