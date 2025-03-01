3b752027d986d76e7dbffc3304ff26042aa81ccb
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 14:37:40 +0530

    Added State for the agent with confirmation

---
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
@@ -0,0 +1,11 @@
+from typing import Annotated
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+    user_info: str
