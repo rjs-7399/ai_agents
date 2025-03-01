18437db3fb2be01ff645b3da32ed5fbe6ab4a86a
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 18:36:08 +0530

    Added State for Multi AI Agent workflow

---
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -0,0 +1,28 @@
+from typing import Annotated, Literal, Optional
+from typing_extensions import TypedDict
+from langgraph.graph.message import AnyMessage, add_messages
+
+
+def updated_dialog_stack(left: list[str], right: Optional[str]) -> list[str]:
+    if right is None:
+        return left
+    if right == "pop":
+        return left[:-1]
+    return left + [right]
+
+
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_messages]
+    user_info: str
+    dialog_state: Annotated[
+        list[
+            Literal[
+                "assistant",
+                "update_flight",
+                "book_car_rental",
+                "book_hotel",
+                "book_excursion",
+            ]
+        ],
+        updated_dialog_stack,
+    ]
