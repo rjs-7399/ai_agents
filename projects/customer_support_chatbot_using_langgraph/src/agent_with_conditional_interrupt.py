006d8ab692bec5371e3c6b5a2be92360d8051e5f
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 16:24:57 +0530

    Added assistant prompt

--- projects/customer_support_chatbot_using_langgraph/src/agent_with_conditional_interrupt.py
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_conditional_interrupt.py
@@ -5,6 +5,26 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable, RunnableConfig
 from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
+from datetime import datetime
+from utils.tools import (
+    fetch_user_flight_information,
+    search_flights,
+    lookup_policy,
+    update_ticket_to_new_flight,
+    cancel_ticket,
+    search_car_rentals,
+    book_car_rental,
+    update_car_rental,
+    cancel_car_rental,
+    search_hotels,
+    book_hotel,
+    update_hotel,
+    cancel_hotel,
+    search_trip_recommendations,
+    book_excursion,
+    update_excursion,
+    cancel_excursion,
+)
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
@@ -33,3 +53,19 @@ llm = ChatOpenAI(
     temperature=0.0,
     model="gpt-4o-mini"
 )
+
+assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
