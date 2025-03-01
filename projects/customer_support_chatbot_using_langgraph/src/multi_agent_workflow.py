3390a93c10ba17ed6accfc4d28b54a86372e98de
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 19:38:14 +0530

    Added Flight booking prompt

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -4,6 +4,7 @@ from langgraph.graph.message import AnyMessage, add_messages
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import Runnable, RunnableConfig
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 
 def updated_dialog_stack(left: list[str], right: Optional[str]) -> list[str]:
@@ -74,3 +75,24 @@ class CompleteOrEscalate(BaseModel):
                 "reason": "I need to seach the user's email or calendar for more information",
             }
         }
+
+
+flight_booking_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling flight updates. "
+            " The primary assistant delegates work to you whenever the user needs help updating their bookings. "
+            "Confirm the updated flight details with the customer and inform them of any additional fees. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\n\nCurrent user flight information:\n<Flights>\n{user_info}\n</Flights>"
+            "\nCurrent time: {time}."
+            "\n\nIf the user needs help, and none of your tools are appropriate for it, then"
+            ' "CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.',
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
