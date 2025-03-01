0e264c4916f5e9c10f68fce1d1a432d7ef8ccc62
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 20:00:11 +0530

    Added Car Rental Booking Prompt

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -171,3 +171,28 @@ hotel_booking_tools = hotel_booking_safe_tools + hotel_booking_sensitive_tools
 hotel_booking_runnable = hotel_booking_prompt | llm.bind_tools(
     hotel_booking_tools + [CompleteOrEscalate]
 )
+
+
+car_rental_booking_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling car rental bookings. "
+            "The primary assistant delegates work to you whenever the user needs help booking a car rental. "
+            "Search for available car rentals based on the user's preferences and confirm the booking details with the customer. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            "\n\nIf the user needs help, and none of your tools are appropriate for it, then "
+            '"CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.'
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'what's the weather like this time of year?'\n"
+            " - 'What flights are available?'\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Car rental booking confirmed'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
