46b871a64cafa809838e9d3ea75e5d350a9357f3
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 19:33:24 +0530

    Added Assistant

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -1,6 +1,8 @@
 from typing import Annotated, Literal, Optional
 from typing_extensions import TypedDict
 from langgraph.graph.message import AnyMessage, add_messages
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import Runnable, RunnableConfig
 from pydantic import BaseModel, Field
 
 
@@ -29,6 +31,26 @@ class State(TypedDict):
     ]
 
 
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            result = self.runnable.invoke(state)
+
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with a real output.")]
+                state = {**state, "messages": messages}
+            else:
+                break
+            return {"messages": result}
+
+
 class CompleteOrEscalate(BaseModel):
     """
         A tool to mark the current task as completed and/or to escalate control of the dialog to the main assistant,
@@ -52,4 +74,3 @@ class CompleteOrEscalate(BaseModel):
                 "reason": "I need to seach the user's email or calendar for more information",
             }
         }
-
