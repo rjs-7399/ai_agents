221e3690553705a6ea711a54db6bead31ac7dd7c
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 14:52:02 +0530

    Added assistant prompt

--- projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_confirmation.py
@@ -54,3 +54,19 @@ llm = ChatOpenAI(
     temperature=0.0,
     model="gpt-4o-mini"
 )
+
+
+assistant_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful customer support assistant for Swiss Airlines. "
+            " Use the provided tools to search for flights, company policies, and other information to assist the user's queries. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " If a search comes up empty, expand your search before giving up."
+            "\n\nCurrent user:\n<User>\n{user_info}\n</User>"
+            "\nCurrent time: {time}.",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
