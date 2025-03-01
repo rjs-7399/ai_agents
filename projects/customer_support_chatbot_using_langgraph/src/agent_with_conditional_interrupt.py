f66da587daf6975ae48a93204578b3a751e5e29d
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 16:16:12 +0530

    Added Assistant in Agent with conditional interrupt

--- projects/customer_support_chatbot_using_langgraph/src/agent_with_conditional_interrupt.py
+++ projects/customer_support_chatbot_using_langgraph/src/agent_with_conditional_interrupt.py
@@ -9,3 +9,22 @@ from langgraph.graph.message import AnyMessage, add_messages
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
     user_info: str
+
+
+class Assistant:
+    def __init__(self, runnable: Runnable):
+        self.runnable = runnable
+
+    def __call__(self, state: State, config: RunnableConfig):
+        while True:
+            result = self.runnable.invoke(state)
+            if not result.tool_calls and (
+                not result.content
+                or isinstance(result.content, list)
+                and not result.content[0].get("text")
+            ):
+                messages = state["messages"] + [("user", "Respond with real output")]
+                state = {**state, "messages": messages}
+            else:
+                break
+        return {"messages": result}
