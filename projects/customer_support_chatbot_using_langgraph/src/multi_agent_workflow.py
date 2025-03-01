378da2a7423148ef10a24f4ee02b9034dbbb974e
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 19:48:10 +0530

    Added Hotel Booking Prompt

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -131,3 +131,29 @@ flight_booking_sensitive_tools = [
 flight_booking_tools = flight_booking_safe_tools + flight_booking_sensitive_tools
 
 flight_booking_runnable = flight_booking_prompt | llm.bind_tools(flight_booking_tools)
+
+
+hotel_booking_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling hotel bookings. "
+            "The primary assistant delegates work to you whenever the user needs help booking a hotel. "
+            "Search for available hotels based on the user's preferences and confirm the booking details with the customer. "
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            '\n\nIf the user needs help, and none of your tools are appropriate for it, then "CompleteOrEscalate" the dialog to the host assistant.'
+            " Do not waste the user's time. Do not make up invalid tools or functions."
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'what's the weather like this time of year?'\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'i need to figure out transportation while i'm there'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Hotel booking confirmed'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
