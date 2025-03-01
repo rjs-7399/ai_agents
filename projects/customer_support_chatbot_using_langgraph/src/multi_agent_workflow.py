6859ac16a9584b61d2ab462fb3db7e6a878f5802
rutvikshah <rutvikjshah@gmail.com>
2025-03-01 20:05:59 +0530

    Added Excursion Booking Prompt

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -208,3 +208,27 @@ car_rental_booking_tools = car_rental_booking_safe_tools + car_rental_booking_se
 car_rental_booking_runnable = car_rental_booking_prompt | llm.bind_tools(
     car_rental_booking_tools + [CompleteOrEscalate]
 )
+
+
+excursion_booking_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a specialized assistant for handling trip recommendations. "
+            "The primary assistant delegates work to you whenever the user needs help booking a recommended trip. "
+            "Search for available trip recommendations based on the user's preferences and confirm the booking details with the customer. "
+            "If you need more information or the customer changes their mind, escalate the task back to the main assistant."
+            " When searching, be persistent. Expand your query bounds if the first search returns no results. "
+            " Remember that a booking isn't completed until after the relevant tool has successfully been used."
+            "\nCurrent time: {time}."
+            '\n\nIf the user needs help, and none of your tools are appropriate for it, then "CompleteOrEscalate" the dialog to the host assistant. Do not waste the user\'s time. Do not make up invalid tools or functions.'
+            "\n\nSome examples for which you should CompleteOrEscalate:\n"
+            " - 'nevermind i think I'll book separately'\n"
+            " - 'i need to figure out transportation while i'm there'\n"
+            " - 'Oh wait i haven't booked my flight yet i'll do that first'\n"
+            " - 'Excursion booking confirmed!'",
+        ),
+        ("placeholder", "{messages}"),
+    ]
+).partial(time=datetime.now)
+
