eb39628ff5004adbf731778595e1319bd2a00a23
rutvikshah <rutvikjshah@gmail.com>
2025-03-02 17:09:58 +0530

    Added Complete Builder for Hotel Booking

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -493,3 +493,26 @@ builder.add_node(
     "book_hotel_sensitive_tools",
     create_tool_node_with_fallback(hotel_booking_sensitive_tools)
 )
+
+
+def route_book_hotel(state: State):
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+    if did_cancel:
+        return "leave_skill"
+    tool_names = [tool.name for tool in hotel_booking_safe_tools]
+    if all(tc["name"] in tool_names for tc in tool_calls):
+        return "book_hotel_sensitive_tools"
+    return "book_hotel_safe_tools"
+
+
+builder.add_edge("book_hotel_sensitive_tools", "book_hotel")
+builder.add_edge("book_hotel_safe_tools", "book_hotel")
+builder.add_conditional_edges(
+    "book_hotel",
+    route_book_hotel,
+    ["leave_skill", "book_hotel_safe_tool", "book_hotel_sensitive_tools", END],
+)
