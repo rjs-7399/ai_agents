6053a195a78a64b7a2493139da639cc0f32346ae
rutvikshah <rutvikjshah@gmail.com>
2025-03-02 16:50:51 +0530

    Added Router for Car Rental

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -450,3 +450,17 @@ builder.add_node(
     "book_car_rental_sensitive_tools",
     create_tool_node_with_fallback(car_rental_booking_sensitive_tools),
 )
+
+
+def route_book_car_rental(state: State):
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+    if did_cancel:
+        return "leave_skill"
+    safe_tool_names = [tool.name for tool in flight_booking_safe_tools]
+    if all(tc["name"] in safe_tool_names for tc in tool_calls):
+        return "book_car_rental_safe_tools"
+    return "book_car_rental_sensitive_tools"
