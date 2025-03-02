e7eef09dbab15abac913f79a749385a2b89c307a
rutvikshah <rutvikjshah@gmail.com>
2025-03-02 18:30:25 +0530

    Added Final Conversation to Multi AI Agent

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -642,3 +642,59 @@ config = {
         "thread_id": thread_id,
     }
 }
+
+
+multi_agent_workflow_questions = [
+    "Hi there, what time is my flight?",
+    "Am i allowed to update my flight to something sooner? I want to leave later today.",
+    "Update my flight to sometime next week then",
+    "The next available option is great",
+    "what about lodging and transportation?",
+    "Yeah i think i'd like an affordable hotel for my week-long stay (7 days). And I'll want to rent a car.",
+    "OK could you place a reservation for your recommended hotel? It sounds nice.",
+    "yes go ahead and book anything that's moderate expense and has availability.",
+    "Now for a car, what are my options?",
+    "Awesome let's just get the cheapest option. Go ahead and book for 7 days",
+    "Cool so now what recommendations do you have on excursions?",
+    "Are they available while I'm there?",
+    "interesting - i like the museums, what options are there? ",
+    "OK great pick one and book it for my second day there.",
+]
+
+_printed = set()
+
+from utils.tools import _print_event
+
+for question in multi_agent_workflow_questions:
+    events = graph.stream(
+        {"messages": ("user", question)}, config, stream_mode="values"
+    )
+    for event in events:
+        _print_event(event, _printed)
+    snapshot = graph.get_state(config)
+    while snapshot.next:
+        try:
+            user_input = input(
+                "Do you approve the above actions ? Type 'y' to continue;"
+                " otherwise, explain your requested chaged.\n\n"
+            )
+        except:
+            user_input = "y"
+        if user_input.strip() == "y":
+            result = graph.invoke(
+                None,
+                config
+            )
+        else:
+            result = graph.invoke(
+                {
+                    "messages": [
+                        ToolMessage(
+                            tool_call_id=event["messages"][-1].tool_calls[0]["id"],
+                            content=f"API call denied by user. Reasoning: '{user_input}'. Continue assisting, accounting for the user's input.",
+                        )
+                    ]
+                },
+                config
+            )
+        snapshot = graph.get_state(config)
