06f8302f2e93a858a4e5f6161a3a4b2f319c3cb3
rutvikshah <rutvikjshah@gmail.com>
2025-03-02 17:24:06 +0530

    Added Router for Book Excursion

--- projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
+++ projects/customer_support_chatbot_using_langgraph/src/multi_agent_workflow.py
@@ -532,3 +532,17 @@ builder.add_node(
     "book_excursion_sensitive_tools",
     create_tool_node_with_fallback(excursion_booking_sensitive_tools)
 )
+
+
+def route_book_excursion(state: State):
+    route = tools_condition(state)
+    if route == END:
+        return END
+    tool_calls = state["messages"][-1].tool_calls
+    did_cancel = any(tc["name"] == CompleteOrEscalate.__name__ for tc in tool_calls)
+    if did_cancel:
+        return "leave_skill"
+    tool_names = [tool.name for tool in excursion_booking_safe_tools]
+    if all(tc["name"] in tool_names for tc in tool_calls):
+        return "book_excursion_safe_tools"
+    return "book_excursion_sensitive_tools"
